ea09c72f1a57e860eced9916785b7a859f8622fe
Daniel Berkness <berknessdanielr@gmail.com>
2022-06-06 19:59:30 -0500

    Initial Commit

---
+++ src/test/java/com/shared/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shared.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
