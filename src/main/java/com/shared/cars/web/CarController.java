9c18f7d740326b8e1316a81da5ff763db3b37746
Daniel Berkness <berknessdanielr@gmail.com>
2022-06-13 19:36:37 -0500

    Created controller

---
+++ src/main/java/com/shared/cars/web/CarController.java
@@ -0,0 +1,15 @@
+package com.shared.cars.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarController {
+	
+	@GetMapping("/create")
+	public String createCar() {
+		
+		return "create";
+	}
+
+}
