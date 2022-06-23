d925826ac82db204d335365c7de231ffe1f206f1
DESKTOP-M11KKJ7\JonBe <jberkness16@winona.edu>
2022-06-22 19:51:49 -0500

    added a table

--- src/main/java/com/shared/cars/web/CarController.java
+++ src/main/java/com/shared/cars/web/CarController.java
@@ -1,7 +1,12 @@
 package com.shared.cars.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CarController {
@@ -28,4 +33,17 @@ public class CarController {
 		
 		return "cars";
 	}
+	@RequestMapping(value = "/showTableWithValues", method = RequestMethod.GET)
+    public String showTableWithValues(Model model) 
+    {
+        //list with Persons
+        ArrayList<Cars>  carsList= 
+                new ArrayList<Cars>();
+
+        carsList=  this.getListOfCars();
+
+        model.addAttribute("list", carsList);
+
+        return "showTableWithValues";
+    }
 }
