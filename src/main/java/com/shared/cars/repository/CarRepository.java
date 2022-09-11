a980afad75c66e9ae5fdef7739501316392e21a3
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2022-09-10 18:55:08 -0700

    Creating update method to save a Car

--- src/main/java/com/shared/cars/repository/CarRepository.java
+++ src/main/java/com/shared/cars/repository/CarRepository.java
@@ -26,13 +26,36 @@ private List<Car> cars = new ArrayList<>(100);
 	}
 
 	public void save(Car car) {
-		if (cars.size() == 0) {// I copied pasted the code above since in order to crea the 10 deafults you need to go to /cars page first
-								//so if i save a car before loading the car page it wouldnt save create the default 10
-			cars.addAll(seedService.carCreation());
-		}//test
-		Long id = (long) cars.size();
-		car.setId(++id);
-		cars.add(car);
+		if(car.getId() != null) {
+			int id = car.getId().intValue();
+			Car carFound = cars.get(id);
+			carFound.setColor(car.getColor());
+			carFound.setModelName(car.getModelName());
+			carFound.setMotorSize(car.getMotorSize());
+			carFound.setPrice(car.getPrice());
+			carFound.setTransmission(car.getTransmission());
+			carFound.setWheelSize(car.getWheelSize());
+			carFound.setYear(car.getYear());
+		}else {
+			if (cars.size() == 0) {// I copied pasted the code above since in order to crea the 10 deafults you need to go to /cars page first
+				//so if i save a car before loading the car page it wouldnt save create the default 10
+				cars.addAll(seedService.carCreation());
+			}//test
+			Long id = (long) cars.size();
+			car.setId(++id);
+			cars.add(car);
+		}
+	}
+
+	public Car findCarById(Long carId) {
+		Car foundCar = new Car();
+		for(Car car : cars) {
+			if(car.getId() == carId) {
+				foundCar = car;
+				break;
+			}
+		}
+		return foundCar;
 	}
 }
 
--- src/main/java/com/shared/cars/web/CarController.java
+++ src/main/java/com/shared/cars/web/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shared.cars.domain.Car;
@@ -46,11 +47,25 @@ public class CarController {
 	
 	@GetMapping("/cars")
     public String allCars(Model model) {
+		
         //list with Cars
         List<Car>  carsList = carService.findAll();
-       
         model.addAttribute("list", carsList);
 
         return "cars";
     }
+	@GetMapping("/car/{carId}")
+	public String viewCar(@PathVariable Long carId, ModelMap model) {
+		Car car = carService.findCarById(carId);
+		System.out.println(car.getMotorSize());
+		model.put("car", car);
+		return "car";
+	}
+	@PostMapping("/car/{carId}")
+	public String updateCar(Car car) {
+		System.out.println(car.getColor());
+		carService.save(car);
+		return "redirect:/cars";
+	}
+	
 }
